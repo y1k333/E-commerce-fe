b357872a8e4aa2fb8579405ba987a0e7213c96cf
KaiYang <kaiyang@usc.edu>
2018-01-30 16:53:51 -0800

    webpack-setup

---
+++ dist/js/index.js
@@ -0,0 +1,19 @@
+webpackJsonp([1],{
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(76);
+
+
+/***/ }),
+
+/***/ 76:
+/***/ (function(module, exports) {
+
+	
+	console.log(index);
+
+/***/ })
+
+});
---
+++ dist/js/login.js
@@ -0,0 +1,19 @@
+webpackJsonp([2],{
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(77);
+
+
+/***/ }),
+
+/***/ 77:
+/***/ (function(module, exports) {
+
+	
+	console.log(login);
+
+/***/ })
+
+});
